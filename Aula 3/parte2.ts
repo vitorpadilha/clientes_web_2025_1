6174c5c7f73ed8d89365e35d5a3827ccf292d498
Your Name <you@example.com>
2025-04-10 20:24:52 -0300

    Aula 3

---
+++ Aula 3/parte2.ts
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(event) {
+    var nome: HTMLInputElement = document.getElementById("nome") as HTMLInputElement;
+    var sobrenome: HTMLInputElement = <HTMLInputElement>document.getElementById("sobrenome");
+    var btnMostrarNomeCompleto: HTMLButtonElement = document.getElementById("btnMostrarNomeCompleto") as HTMLButtonElement;
+    var nomeCompleto: HTMLInputElement = document.getElementById("nomeCompleto") as HTMLInputElement;
+
+    btnMostrarNomeCompleto.addEventListener("click", function(event){
+        
+    });
+});
