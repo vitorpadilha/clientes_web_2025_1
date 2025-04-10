3e0c45de94178f31d0fe67bd2228f93bf24ec621
Your Name <you@example.com>
2025-04-10 19:38:05 -0300

    Aula 3

--- Aula 3/dist/parte1.js
+++ Aula 3/dist/parte1.js
@@ -4,18 +4,33 @@ class Pessoa {
         this.nome = nome;
         this.sobrenome = sobrenome;
     }
+    nomeCompleto() {
+        return this.nome + " " + this.sobrenome;
+    }
 }
 //Declarando e iniciando objeto e fazendo CAST para tipo Pessoa com a palavra reservada "as"
 var pessoa1 = {
     nome: "Fulano",
     sobrenome: "Da Silva Knupp",
-    dataNascimento: "15/03/2001"
+    dataNascimento: "15/03/2001",
+    nomeCompleto() {
+        return this.nome + " " + this.sobrenome;
+    }
 };
 //Declarando e iniciando objeto e fazendo CAST para tipo Pessoa com sinal de maior e menor
 var pessoa2 = {
     nome: "Ciclano",
     sobrenome: "Da Silva Knupp",
-    dataNascimento: "14/03/2001"
+    dataNascimento: "14/03/2001",
+    nomeCompleto() {
+        return this.nome + " " + this.sobrenome;
+    }
 };
 //Declarando e iniciando objeto (método tradicional, Java)
 var pessoa3 = new Pessoa("Daniel", "Souza");
+for (var prop in pessoa2) {
+    var key = prop;
+    console.log(typeof pessoa2[key]);
+    console.log("Nome Propriedade:", prop);
+    console.log("Valor:", pessoa2[key]);
+}
--- Aula 3/parte1.ts
+++ Aula 3/parte1.ts
@@ -1,23 +1,45 @@
 class Pessoa {
-    nome: string;
-    sobrenome: string;
+    public nome: string;
+    public sobrenome: string;
+    //Também existe o private e protected
     constructor(nome: string, sobrenome: string) {
         this.nome = nome;
         this.sobrenome = sobrenome;
+    }
+    nomeCompleto(): string {
+        return this.nome+" "+this.sobrenome;
     }
 }
 //Declarando e iniciando objeto e fazendo CAST para tipo Pessoa com a palavra reservada "as"
 var pessoa1: Pessoa = {
     nome: "Fulano",
     sobrenome: "Da Silva Knupp",
-    dataNascimento: "15/03/2001"
+    dataNascimento: "15/03/2001",
+    nomeCompleto(): string {
+        return this.nome+" "+this.sobrenome;
+    }
 } as Pessoa;
 
 //Declarando e iniciando objeto e fazendo CAST para tipo Pessoa com sinal de maior e menor
 var pessoa2: Pessoa = <Pessoa>{
     nome: "Ciclano",
     sobrenome: "Da Silva Knupp",
-    dataNascimento: "14/03/2001"
+    dataNascimento: "14/03/2001",
+    nomeCompleto(): string {
+        return this.nome+" "+this.sobrenome;
+    }
 };
 //Declarando e iniciando objeto (método tradicional, Java)
 var pessoa3: Pessoa = new Pessoa("Daniel", "Souza");
+
+for(var prop in pessoa2) {
+    var key = prop as keyof Pessoa;
+    if(typeof pessoa2[key] =="function") {
+        console.log("Nome Função:", prop);
+        console.log("Valor:", pessoa2[key]);
+    }
+    else {
+        console.log("Nome Propriedade:", prop);
+        console.log("Valor:", pessoa2[key]);
+    }   
+}
