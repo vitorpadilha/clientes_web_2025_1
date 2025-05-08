9b0cb8c0a6d21c9960bd8fa91a07e6c27705b675
Your Name <you@example.com>
2025-05-08 20:37:51 -0300

    Aula 6

--- Aula 6/atividade2.ts
+++ Aula 6/atividade2.ts
@@ -1,3 +1,36 @@
+const categorias = ["Limpeza", "Bomboniere", "Cereais"]
 document.addEventListener("DOMContentLoaded", (ev)=>{
 
 });
+let contador = 1;
+let adicionaCategoria = (valor: number)=>{
+    contador++;
+    let divCategorias = document.getElementById("categorias") as HTMLDivElement;
+
+    let divWrapperCategoria = document.createElement("div");
+    divWrapperCategoria.id = "categoria"+contador;
+    let selectCategoria = document.createElement("select");
+    divWrapperCategoria.appendChild(selectCategoria);
+
+    divCategorias.appendChild(divWrapperCategoria);
+    adicionaOptions(selectCategoria, categorias);
+
+    let divWrapperBotaoRemoverCategoria = document.createElement("div");
+    divWrapperBotaoRemoverCategoria.setAttribute("name", "btnSubCategoria");
+
+
+    let divWrapperBtnClicado = document.getElementById("categoria"+valor);
+    for(let childDiv of divCategorias.getElementsByTagName("div")){
+        let childDivElement = childDiv as HTMLElement;
+        
+    }
+}
+
+let adicionaOptions = (campo: HTMLSelectElement, valores: string[]) => {
+    for( let valor of valores) {
+        let opt = document.createElement("option");
+        opt.value = valor;
+        opt.textContent = valor;
+        campo.appendChild(opt); 
+    }
+}
--- Aula 6/dist/atividade2.js
+++ Aula 6/dist/atividade2.js
@@ -1,1 +1,10 @@
 "use strict";
+document.addEventListener("DOMContentLoaded", (ev) => {
+});
+let adicionaCategoria = (valor) => {
+    let divCategorias = document.getElementById("categorias");
+    for (let child of divCategorias.children) {
+        let childElement = child;
+        console.log(childElement.getAttribute("type"));
+    }
+};
