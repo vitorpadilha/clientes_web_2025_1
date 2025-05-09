bfb50b66d1bafa0aa9ad18892f070fb0494cc11c
Your Name <you@example.com>
2025-05-08 21:08:10 -0300

    Aula 6

--- Aula 6/atividade2.ts
+++ Aula 6/atividade2.ts
@@ -1,32 +1,72 @@
-const categorias = ["Limpeza", "Bomboniere", "Cereais"]
-document.addEventListener("DOMContentLoaded", (ev)=>{
+const categorias = ["Limpeza", "Bomboniere", "Cereais"];
 
-});
 let contador = 1;
+let contadorPresentesAtual = 1;
 let adicionaCategoria = (valor: number)=>{
     contador++;
-    let divCategorias = document.getElementById("categorias") as HTMLDivElement;
+    
+    let fieldSetCategorias = document.getElementById("categorias") as HTMLFieldSetElement;
 
     let divWrapperCategoria = document.createElement("div");
     divWrapperCategoria.id = "categoria"+contador;
     let selectCategoria = document.createElement("select");
     divWrapperCategoria.appendChild(selectCategoria);
 
-    divCategorias.appendChild(divWrapperCategoria);
-    adicionaOptions(selectCategoria, categorias);
+    fieldSetCategorias.appendChild(divWrapperCategoria);
+    adicionaOptions(selectCategoria, categorias, "-- Selecione a Categoria --");
 
     let divWrapperBotaoRemoverCategoria = document.createElement("div");
     divWrapperBotaoRemoverCategoria.setAttribute("name", "btnSubCategoria");
+    divWrapperBotaoRemoverCategoria.id = "btnSubCategoria"+contador;
 
+    let btnRemoveCategoria = document.createElement("button");
+    btnRemoveCategoria.textContent="-";
+    btnRemoveCategoria.type = "button";
+    btnRemoveCategoria.addEventListener("click",(ev)=>{
+        removerCategoria(contador);
+    });
+    divWrapperBotaoRemoverCategoria.appendChild(btnRemoveCategoria);
 
-    let divWrapperBtnClicado = document.getElementById("categoria"+valor);
-    for(let childDiv of divCategorias.getElementsByTagName("div")){
+    let divWrapperBotaoAdicionarCategoria = document.createElement("div");
+    divWrapperBotaoAdicionarCategoria.setAttribute("name", "btnSomaCategoria");
+    divWrapperBotaoAdicionarCategoria.id = "btnSubCategoria"+contador;
+
+    let btnAdicionaCategoria = document.createElement("button");
+    btnAdicionaCategoria.textContent="+";
+    btnAdicionaCategoria.type = "button";
+    btnAdicionaCategoria.addEventListener("click",(ev)=>{
+        adicionaCategoria(contador);
+    });
+    divWrapperBotaoAdicionarCategoria.appendChild(btnAdicionaCategoria);
+    divWrapperCategoria.appendChild(divWrapperBotaoRemoverCategoria);
+    divWrapperCategoria.appendChild(divWrapperBotaoAdicionarCategoria);
+
+    let divWrapperBtnClicado = document.getElementById("categoria"+valor) as HTMLDivElement;
+    for(let childDiv of divWrapperBtnClicado?.getElementsByTagName("div")){
         let childDivElement = childDiv as HTMLElement;
-        
+        if(childDivElement.getAttribute("name") == "btnSomaCategoria") {
+            divWrapperBtnClicado.removeChild(childDiv);
+        }
+        else if(contadorPresentesAtual==1 && childDiv.getAttribute("name") == "btnSubCategoria") {
+            let btnRemoveCategoriaInterno = document.createElement("button");
+            btnRemoveCategoriaInterno.textContent="-";
+            btnRemoveCategoriaInterno.type = "button";
+            btnRemoveCategoriaInterno.addEventListener("click",(ev)=>{
+                removerCategoria(valor);
+            });
+            childDiv.appendChild(btnRemoveCategoria);
+        }
     }
+   contadorPresentesAtual++;
 }
-
-let adicionaOptions = (campo: HTMLSelectElement, valores: string[]) => {
+let removerCategoria = (valor: number)=>{
+    contadorPresentesAtual--;
+}
+let adicionaOptions = (campo: HTMLSelectElement, valores: string[], textoOptionVazio: string) => {
+    let opt = document.createElement("option");
+    opt.value = "";
+    opt.textContent = textoOptionVazio;
+    campo.appendChild(opt);
     for( let valor of valores) {
         let opt = document.createElement("option");
         opt.value = valor;
--- Aula 6/dist/atividade2.js
+++ Aula 6/dist/atividade2.js
@@ -1,10 +1,67 @@
 "use strict";
+const categorias = ["Limpeza", "Bomboniere", "Cereais"];
 document.addEventListener("DOMContentLoaded", (ev) => {
 });
+let contador = 1;
 let adicionaCategoria = (valor) => {
+    contador++;
     let divCategorias = document.getElementById("categorias");
-    for (let child of divCategorias.children) {
-        let childElement = child;
-        console.log(childElement.getAttribute("type"));
+    let divWrapperCategoria = document.createElement("div");
+    divWrapperCategoria.id = "categoria" + contador;
+    let selectCategoria = document.createElement("select");
+    divWrapperCategoria.appendChild(selectCategoria);
+    divCategorias.appendChild(divWrapperCategoria);
+    adicionaOptions(selectCategoria, categorias, "-- Selecione a Categoria --");
+    let divWrapperBotaoRemoverCategoria = document.createElement("div");
+    divWrapperBotaoRemoverCategoria.setAttribute("name", "btnSubCategoria");
+    divWrapperBotaoRemoverCategoria.id = "btnSubCategoria" + contador;
+    let btnRemoveCategoria = document.createElement("button");
+    btnRemoveCategoria.textContent = "-";
+    btnRemoveCategoria.type = "button";
+    btnRemoveCategoria.addEventListener("click", (ev) => {
+        removerCategoria(contador);
+    });
+    divWrapperBotaoRemoverCategoria.appendChild(btnRemoveCategoria);
+    let divWrapperBotaoAdicionarCategoria = document.createElement("div");
+    divWrapperBotaoAdicionarCategoria.setAttribute("name", "btnSomaCategoria");
+    divWrapperBotaoAdicionarCategoria.id = "btnSubCategoria" + contador;
+    let btnAdicionaCategoria = document.createElement("button");
+    btnAdicionaCategoria.textContent = "+";
+    btnAdicionaCategoria.type = "button";
+    btnAdicionaCategoria.addEventListener("click", (ev) => {
+        adicionaCategoria(contador);
+    });
+    divWrapperBotaoAdicionarCategoria.appendChild(btnAdicionaCategoria);
+    divCategorias.appendChild(divWrapperBotaoRemoverCategoria);
+    divCategorias.appendChild(divWrapperBotaoAdicionarCategoria);
+    let divWrapperBtnClicado = document.getElementById("categoria" + valor);
+    for (let childDiv of divWrapperBtnClicado === null || divWrapperBtnClicado === void 0 ? void 0 : divWrapperBtnClicado.getElementsByTagName("div")) {
+        let childDivElement = childDiv;
+        if (childDiv.getAttribute("name") == "btnSomaCategoria") {
+            divWrapperBtnClicado.removeChild(childDiv);
+        }
+        else if (valor == 1 && childDiv.getAttribute("name") == "btnSubCategoria") {
+            let btnRemoveCategoriaInterno = document.createElement("button");
+            btnRemoveCategoriaInterno.textContent = "-";
+            btnRemoveCategoriaInterno.type = "button";
+            btnRemoveCategoriaInterno.addEventListener("click", (ev) => {
+                removerCategoria(valor);
+            });
+            childDiv.appendChild(btnRemoveCategoria);
+        }
+    }
+};
+let removerCategoria = (valor) => {
+};
+let adicionaOptions = (campo, valores, textoOptionVazio) => {
+    let opt = document.createElement("option");
+    opt.value = "";
+    opt.textContent = textoOptionVazio;
+    campo.appendChild(opt);
+    for (let valor of valores) {
+        let opt = document.createElement("option");
+        opt.value = valor;
+        opt.textContent = valor;
+        campo.appendChild(opt);
     }
 };
