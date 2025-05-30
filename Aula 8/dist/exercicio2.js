05b5c100c64782b0b8e1c376ec9a9d6303d44a3b
Your Name <you@example.com>
2025-05-30 16:14:27 -0300

    Aula 8

--- Aula 8/dist/exercicio2.js
+++ Aula 8/dist/exercicio2.js
@@ -1,14 +1,20 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", (ev) => {
-    var _a;
+    var _a, _b;
     (_a = document.getElementById("btnEnviar")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", (ev1) => {
         adicionarNomeNoLocalStorage();
-        montarTabela();
+    });
+    (_b = document.getElementById("btnFiltrar")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (ev1) => {
+        var _a;
+        montarTabela((_a = document.getElementById("filtro")) === null || _a === void 0 ? void 0 : _a.value);
     });
 });
-let montarTabela = () => {
+let montarTabela = (filtro) => {
     var _a;
     let nomes = localStorage.getItem("nomes") ? JSON.parse((_a = localStorage.getItem("nomes")) !== null && _a !== void 0 ? _a : "") : [];
+    nomes = nomes.filter((valor) => {
+        return valor.toLowerCase().includes(filtro.toLowerCase());
+    });
     const caixaTexto = document.getElementById("caixaTexto");
     caixaTexto ? (caixaTexto.innerHTML = "") : null;
     const tabela = document.createElement("table");
@@ -19,6 +25,9 @@ let montarTabela = () => {
     const thCabecalho = document.createElement("th");
     thCabecalho.textContent = "Nomes Cadastrados";
     trCabecalho.appendChild(thCabecalho);
+    const thCabecalho2 = document.createElement("th");
+    thCabecalho2.textContent = "Ação";
+    trCabecalho.appendChild(thCabecalho2);
     const tBody = document.createElement("tbody");
     tabela.appendChild(tBody);
     nomes.forEach((valor) => {
@@ -26,9 +35,26 @@ let montarTabela = () => {
         const tdLinha = document.createElement("td");
         tdLinha.textContent = valor;
         trLinha.appendChild(tdLinha);
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir";
+        botaoExcluir.addEventListener("click", (ev1) => {
+            removerItemNoLocalStorage(valor);
+            montarTabela(filtro);
+        });
+        const tdLinhaBotao = document.createElement("td");
+        tdLinhaBotao.appendChild(botaoExcluir);
+        trLinha.appendChild(tdLinhaBotao);
         tBody.appendChild(trLinha);
     });
     caixaTexto === null || caixaTexto === void 0 ? void 0 : caixaTexto.appendChild(tabela);
+};
+let removerItemNoLocalStorage = (nomeEscolhido) => {
+    var _a;
+    let nomes = localStorage.getItem("nomes") ? JSON.parse((_a = localStorage.getItem("nomes")) !== null && _a !== void 0 ? _a : "") : [];
+    nomes = nomes.filter((valor) => {
+        return !(valor === nomeEscolhido);
+    });
+    localStorage.setItem("nomes", JSON.stringify(nomes));
 };
 let adicionarNomeNoLocalStorage = () => {
     var _a, _b;
--- Aula 8/exercicio2.ts
+++ Aula 8/exercicio2.ts
@@ -1,12 +1,17 @@
 document.addEventListener("DOMContentLoaded", (ev)=>{
     document.getElementById("btnEnviar")?.addEventListener("click",(ev1)=>{
         adicionarNomeNoLocalStorage();
-        montarTabela();
-    })
+    });
+    document.getElementById("btnFiltrar")?.addEventListener("click",(ev1)=>{
+        montarTabela((document.getElementById("filtro") as HTMLInputElement)?.value);
+    });
 });
 
-let montarTabela = ()=>{
+let montarTabela = (filtro: string)=>{
     let nomes: string[] = localStorage.getItem("nomes")?JSON.parse(localStorage.getItem("nomes")??""):[];
+    nomes = nomes.filter((valor)=>{
+        return valor.toLowerCase().includes(filtro.toLowerCase());
+    })
     const caixaTexto = document.getElementById("caixaTexto");
     caixaTexto?(caixaTexto.innerHTML=""):null;
     const tabela = document.createElement("table");
@@ -14,11 +19,15 @@ let montarTabela = ()=>{
     tabela.appendChild(thead);
     const trCabecalho = document.createElement("tr");
     thead.appendChild(trCabecalho);
-    const thCabecalho = document.createElement("th");
-    
-    thCabecalho.textContent="Nomes Cadastrados";
-    
+    const thCabecalho = document.createElement("th");    
+    thCabecalho.textContent="Nomes Cadastrados";    
     trCabecalho.appendChild(thCabecalho);
+
+    const thCabecalho2 = document.createElement("th");
+    
+    thCabecalho2.textContent="Ação";
+    
+    trCabecalho.appendChild(thCabecalho2);
     const tBody = document.createElement("tbody");
     tabela.appendChild(tBody);
     nomes.forEach((valor)=>{
@@ -26,10 +35,27 @@ let montarTabela = ()=>{
         const tdLinha = document.createElement("td");
         tdLinha.textContent = valor;
         trLinha.appendChild(tdLinha);
+
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.textContent = "Excluir";
+        botaoExcluir.addEventListener("click",(ev1)=>{
+            removerItemNoLocalStorage(valor);
+            montarTabela(filtro);
+        })
+        const tdLinhaBotao = document.createElement("td");
+        tdLinhaBotao.appendChild(botaoExcluir);
+        trLinha.appendChild(tdLinhaBotao);
         tBody.appendChild(trLinha);
     });
 
     caixaTexto?.appendChild(tabela);
+}
+let removerItemNoLocalStorage = (nomeEscolhido: string) =>{
+    let nomes: string[] = localStorage.getItem("nomes")?JSON.parse(localStorage.getItem("nomes")??""):[];
+    nomes = nomes.filter((valor)=>{
+        return !(valor === nomeEscolhido)
+    });
+    localStorage.setItem("nomes", JSON.stringify(nomes));
 }
 let adicionarNomeNoLocalStorage = ()=>{
     let nomes: string[] = localStorage.getItem("nomes")?JSON.parse(localStorage.getItem("nomes")??""):[];
